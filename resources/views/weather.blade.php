45300cdbae9ba624d6ddc58ba3d8e3b982aac8d9
Steve Chamaillard <steve@kang.fr>
2022-10-05 19:04:06 +0200

    Add weather icons Add readme Integrate weather page

---
+++ resources/views/weather.blade.php
@@ -0,0 +1,219 @@
+<div class="p-10">
+    <div class="p-10 border-solid border-2 border-gray-200">
+        <div>
+            <div>
+                <span class="text-4xl text-gray-500">samedi</span>
+            </div>
+
+            <br>
+
+            <div class="flex items-center">
+                <i class="wi wi-day-cloudy text-7xl text-gray-500 mr-2"></i>
+                <h1 class="text-8xl">15
+                    <sup class="text-3xl align-super -ml-5">°C</sup>
+                </h1>
+            </div>
+        </div>
+
+        <br>
+        <br>
+
+        <div class="flex justify-between px-3">
+            <div class="flex justify-center flex-col items-center">
+                <div>
+                    <span class="text-2xl text-gray-500">13<sup>°C</sup></span>
+                </div>
+                <div>
+                    <span class="text-2xl text-gray-400">01:00</span>
+                </div>
+            </div>
+
+            <div class="flex justify-center flex-col items-center">
+                <div>
+                    <span class="text-2xl text-gray-500">13<sup>°C</sup></span>
+                </div>
+                <div>
+                    <span class="text-2xl text-gray-400">04:00</span>
+                </div>
+            </div>
+
+            <div class="flex justify-center flex-col items-center">
+                <div>
+                    <span class="text-2xl text-gray-500">15<sup>°C</sup></span>
+                </div>
+                <div>
+                    <span class="text-2xl text-gray-400">07:00</span>
+                </div>
+            </div>
+
+            <div class="flex justify-center flex-col items-center">
+                <div>
+                    <span class="text-2xl text-gray-500">15<sup>°C</sup></span>
+                </div>
+                <div>
+                    <span class="text-2xl text-gray-400">10:00</span>
+                </div>
+            </div>
+
+            <div class="flex justify-center flex-col items-center">
+                <div>
+                    <span class="text-2xl text-gray-500">15<sup>°C</sup></span>
+                </div>
+                <div>
+                    <span class="text-2xl text-gray-400">13:00</span>
+                </div>
+            </div>
+
+            <div class="flex justify-center flex-col items-center">
+                <div>
+                    <span class="text-2xl text-gray-500">13<sup>°C</sup></span>
+                </div>
+                <div>
+                    <span class="text-2xl text-gray-400">16:00</span>
+                </div>
+            </div>
+
+            <div class="flex justify-center flex-col items-center">
+                <div>
+                    <span class="text-2xl text-gray-500">13<sup>°C</sup></span>
+                </div>
+                <div>
+                    <span class="text-2xl text-gray-400">19:00</span>
+                </div>
+            </div>
+
+            <div class="flex justify-center flex-col items-center">
+                <div>
+                    <span class="text-2xl text-gray-500">11<sup>°C</sup></span>
+                </div>
+                <div>
+                    <span class="text-2xl text-gray-400">22:00</span>
+                </div>
+            </div>
+        </div>
+
+        <br>
+
+        <div class="flex justify-between">
+            <div class="flex justify-center flex-col items-center gap-y-3">
+                <div>
+                    <span class="text-3xl text-gray-400">ven.</span>
+                </div>
+
+                <div>
+                    <i class="wi wi-day-sunny text-7xl text-yellow-500 mr-2"></i>
+                </div>
+
+                <div class="flex gap-x-2">
+                    <span class="text-2xl text-gray-500">17<sup>°</sup></span>
+                    <span class="text-2xl text-gray-400">12<sup>°</sup></span>
+                </div>
+            </div>
+
+            <div class="flex justify-center flex-col items-center gap-y-3">
+                <div>
+                    <span class="text-3xl text-gray-400">sam.</span>
+                </div>
+
+                <div>
+                    <i class="wi wi-rain text-7xl text-blue-500 mr-2"></i>
+                </div>
+
+                <div class="flex gap-x-2">
+                    <span class="text-2xl text-gray-500">15<sup>°</sup></span>
+                    <span class="text-2xl text-gray-400">11<sup>°</sup></span>
+                </div>
+            </div>
+
+            <div class="flex justify-center flex-col items-center gap-y-3">
+                <div>
+                    <span class="text-3xl text-gray-400">dim.</span>
+                </div>
+
+                <div>
+                    <i class="wi wi-day-cloudy text-7xl text-gray-500 mr-2"></i>
+                </div>
+
+                <div class="flex gap-x-2">
+                    <span class="text-2xl text-gray-500">13<sup>°</sup></span>
+                    <span class="text-2xl text-gray-400">9<sup>°</sup></span>
+                </div>
+            </div>
+
+            <div class="flex justify-center flex-col items-center gap-y-3">
+                <div>
+                    <span class="text-3xl text-gray-400">lun.</span>
+                </div>
+
+                <div>
+                    <i class="wi wi-day-sunny text-7xl text-yellow-500 mr-2"></i>
+                </div>
+
+                <div class="flex gap-x-2">
+                    <span class="text-2xl text-gray-500">15<sup>°</sup></span>
+                    <span class="text-2xl text-gray-400">7<sup>°</sup></span>
+                </div>
+            </div>
+
+            <div class="flex justify-center flex-col items-center gap-y-3">
+                <div>
+                    <span class="text-3xl text-gray-400">mar.</span>
+                </div>
+
+                <div>
+                    <i class="wi wi-rain text-7xl text-blue-500 mr-2"></i>
+                </div>
+
+                <div class="flex gap-x-2">
+                    <span class="text-2xl text-gray-500">16<sup>°</sup></span>
+                    <span class="text-2xl text-gray-400">10<sup>°</sup></span>
+                </div>
+            </div>
+
+            <div class="flex justify-center flex-col items-center gap-y-3">
+                <div>
+                    <span class="text-3xl text-gray-400">mer.</span>
+                </div>
+
+                <div>
+                    <i class="wi wi-day-cloudy text-7xl text-gray-500 mr-2"></i>
+                </div>
+
+                <div class="flex gap-x-2">
+                    <span class="text-2xl text-gray-500">21<sup>°</sup></span>
+                    <span class="text-2xl text-gray-400">12<sup>°</sup></span>
+                </div>
+            </div>
+
+            <div class="flex justify-center flex-col items-center gap-y-3">
+                <div>
+                    <span class="text-3xl text-gray-400">jeu.</span>
+                </div>
+
+                <div>
+                    <i class="wi wi-day-sunny text-7xl text-yellow-500 mr-2"></i>
+                </div>
+
+                <div class="flex gap-x-2">
+                    <span class="text-2xl text-gray-500">19<sup>°</sup></span>
+                    <span class="text-2xl text-gray-400">12<sup>°</sup></span>
+                </div>
+            </div>
+
+            <div class="flex justify-center flex-col items-center gap-y-3">
+                <div>
+                    <span class="text-3xl text-gray-400">ven.</span>
+                </div>
+
+                <div>
+                    <i class="wi wi-rain text-7xl text-blue-500 mr-2"></i>
+                </div>
+
+                <div class="flex gap-x-2">
+                    <span class="text-2xl text-gray-500">18<sup>°</sup></span>
+                    <span class="text-2xl text-gray-400">11<sup>°</sup></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
